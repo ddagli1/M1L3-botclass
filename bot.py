eac154ad0b2036d2437b688bc363a93437e58d2a
Deniz🌊 <kdendag@gmail.com>
2025-11-08 15:52:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,27 @@
 import discord
-from discord.ext import commands
-
+from bot_mantik import gen_pass
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
 intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
 intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
 
-bot = commands.Bot(command_prefix='$', intents=intents)
-
-@bot.event
+@client.event
 async def on_ready():
-    print(f'{bot.user} olarak giriş yaptık')
+    print(f'{client.user} olarak giriş yaptık.')
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$merhaba'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$sifre'):
+        await message.channel.send(gen_pas(10))
+    else:
+        await message.channel.send(message.content)
 
-@bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
-
-bot.run("")
+client.run("BOTUNUZUN TOKEN BURADA OLMALIDIR!")
